f66114697698e3312158d1bc6a2211726879d4cd
LIMPIX31 <limpix31@gmail.com>
2022-04-22 16:44:10 +0300

    init process

---
+++ index.ts
@@ -0,0 +1,55 @@
+import execa from 'execa'
+import { ProcessRunException } from './ProcessRunException'
+
+export const resolveJavaExecutable = (javaPath?: string | null): string => {
+  return javaPath ? `"${javaPath}"` : 'java'
+}
+
+export const checkJava = async (javaPath?: string): Promise<boolean> => {
+  try {
+    const { stderr } = await execa.command(`${resolveJavaExecutable(javaPath)} -version`)
+    return !!stderr
+      .matchAll(
+        /java version "(?<version>.+?)"/g
+      )
+      .next().value?.groups
+  } catch (e) {
+    return false
+  }
+}
+
+const run = (
+  command: string,
+  args: string[],
+  log?: (data: string) => void,
+  cwd?: string
+): Promise<void> => {
+  return new Promise<void>(resolve => {
+    // (err, stdout, stderr) => {
+    //   if (err) throw new ProcessRunException(err.message)
+    //   else resolve()
+    // }
+
+    try {
+      const child = execa(command, args, {
+        cwd: cwd || process.cwd(),
+        stdout: 'inherit',
+        stderr: 'inherit'
+      })
+      if (log) {
+        child.stdout?.on('data', (data: string) => log(data))
+        child.stderr?.on('data', (data: string) => log(data))
+      }
+      child
+        .then(() => resolve())
+        .catch((e: any) => {
+          throw new ProcessRunException(e)
+        })
+    } catch (e: any) {
+      throw new ProcessRunException(e)
+    }
+  })
+}
+
+export const runJava = async (javaPath: string | undefined | null, args: string[], log?: (data: string) => void) =>
+  await run(resolveJavaExecutable(javaPath), args, log)
